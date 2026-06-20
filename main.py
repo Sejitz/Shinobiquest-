1b96e9c382b3003f77f1aa7b85264d4e1a8d5b96
Ankit Roy <gameztthrons@gmail.com>
2026-06-20 19:13:59 +0530

    Update main.py
    Shinobi Quest v2.1

--- main.py
+++ main.py
@@ -1,84 +1,119 @@
 import json # Import JSON module for saving game data
+import random
 
 print("====SHINOBI QUEST====\n1. New Game\n2. Load Game\n3. Exit") # Display the main menu
 
 # ===== CHARACTER CREATION =====
-player_hp = 100
-chakra = 50
+def save_game():
+    with open("save.json","w") as file:
+        json.dump(player, file)
+    
 while True:
     choice = input("Choose: ") # Get the player's menu choice
     if choice == '1': # Start a new game
         name = input("Enter Your Name: ")
         print("\nChoose Village\n1. Leaf\n2. Sand\n3. Mist\n")
         while True:
-            choice2 = input("Choose: ")
+            choice_village = input("Choose: ")
             
-            if choice2 == '1': # Assign the selected village
+            if choice_village == '1': # Assign the selected village
                 village = "Leaf"
                 break
-            elif choice2 == '2':
+            elif choice_village == '2':
                 village = "Sand"
                 break
-            elif choice2 == '3':
+            elif choice_village == '3':
                 village = "Mist"
                 break 
             else:
                 print("Invalid Village input!")
         print("Village selected:", village)
+        # ===== SAVE SYSTEM =====
+        # Create a dictionary containing player data
+        player = {
+            "name": name,
+            "village": village,
+            "level": 1,
+            "hp": 100,
+            "chakra": 50,
+            "xp" : 0
+        }
+        save_game()  # Save player data to a JSON file            
         break
+        
+    elif choice == '2':
+        with open("save.json","r") as file:
+            player = json.load(file)
+            print(f"\nWelcome Back {player['name']}")
+            print(f"Current Village: {player['village']}")
+            print(f"Level: {player['level']}")
+            print(f"HP: {player['hp']}")
+            print(f"Chakra: {player['chakra']}")
+            print(f"XP: {player['xp']}")
+            break
+        
+        
     
-# ===== SAVE SYSTEM =====
-# Create a dictionary containing player data
-player = {
-    "name": name,
-    "village": village,
-    "level": 1,
-    "hp": player_hp,
-    "chakra": chakra
-}
 
-with open("save.json","w") as file: # Save player data to a JSON file
-    json.dump(player, file)
     
 # ===== BATTLE SYSTEM =====
-print("\nYou are Good to go, Start the Journey?\n1. Yes\n2. No")
+print("\nYou are Good to go, Start/Continue the Journey?\n1. Yes\n2. No")
 while True:     # Main game exploration loop
-    choice = input("Choose: ")
-    if choice == "2":
+    choice_journey = input("Choose: ")
+    if choice_journey == "2":
         break
-    elif choice == "1":
+    elif choice_journey == "1":
         while True:     # Exploration menu
             print("\n1. Search\n2. Exit")
-            choice = input("Choose: ")
+            choice_search = input("Choose: ")
             
-            if choice == "1":    # Start a random encounter
+            if choice_search == "1":    # Start a random encounter
+                if player["hp"] <= 0:
+                    print("You can't fight anymore Your hp is 0!\nPlease recover health to continue fight.")
+                    continue
                 bandit_hp = 50
-                print(f"\nA Bandit Appeared!\nYour Hp: {player_hp}\nBandit Hp: {bandit_hp}")
+                print(f"\nA Bandit Appeared!\nYour Hp: {player['hp']}\nBandit Hp: {bandit_hp}")
                     
-                while player_hp >0 and bandit_hp >0: # Battle loop continues while both fighters are alive
+                while player["hp"] >0 and bandit_hp >0: # Battle loop continueswhile both fighters are alive
                     print("\n1. Attack\n2. Run")
-                    choice = input("Choose: ")
-                    if choice == '1':
+                    choice_attack = input("Choose: ")
+                    if choice_attack == '1':
                         bandit_hp = bandit_hp - 10
-                        player_hp = player_hp - 5
-                        print("You dealt 10 damage!")
-                        print("Bandit dealt 5 damage!")
-                        print(f"Your Hp: {player_hp}\nBandit Hp: {bandit_hp}")
+                        player["hp"] -= 5
+                        print("\nYou dealt 10 damage!")
+                        print("Bandit dealt 5 damage!\n")
+                        print(f"Your Hp: {player['hp']}\nBandit Hp: {bandit_hp}")
                         
-                        if player_hp <= 0:
+                        if player["hp"] <= 0:
                             print("You were defeated!")
+                            save_game()
                             break
-                        if bandit_hp <= 0:
+
+                        elif bandit_hp <= 0:
                             print("You defeated the Bandit!!")
+                            # ===== EXPERIENCE REWARD SYSTEM =====
+                            # Grant XP reward for a successful battle
+                            xp_gain = random.randrange(70, 155)
+                            player["xp"] += xp_gain
+                            # Display progression update
+                            print(f"You gained {xp_gain} XP!!")
+                            while player["xp"] >= 500:
+                                player["level"] += 1
+                                player["xp"] -= 500  #stores the remaining xp after levelling up
+                                print("\n--==Level Up==--")
+                                print(f"You reached Level {player['level']} !!")
+                            save_game()
                             break
-                        # Update saved HP after battle progress
-                        player["hp"] = player_hp
-                        with open("save.json","w") as file:
-                            json.dump(player, file)
+                        # Update saved HP Level and xp after battle progress
                             
-                    elif choice == '2':
+                    elif choice_attack == '2':
                         print("You escaped the battle!")
+                        save_game()
                         break
-            elif choice == "2":
+                    
+                        
+            elif choice_search == "2":
                 break
-    break
+    break #while searching if player exit search ,this 'break' end the whole game
+
+  
